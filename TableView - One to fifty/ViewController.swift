15c6b17c87820d2e7ac8afcedfc1be46f6501ffe
elnatan <elnatan@ravtech.co.il>
2020-01-23 13:18:18 +0200

    Initial Commit

---
+++ TableView - One to fifty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView - One to fifty
+//
+//  Created by admin on 23/01/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
